578d57655825443758411f4efe578137da53896c
TonyStark1425 <halfbloodprince0714@gmail.com>
2025-06-25 23:04:50 +0530

    Update Operators.py

--- Chapter 2/Operators.py
+++ Chapter 2/Operators.py
@@ -1,31 +1,31 @@
 # Arithmatic 
 
-# a = 7
-# b = 4
-# c = a + b
-# print(c)
+a = 7
+b = 4
+c = a + b
+print(c)
 
 # # Assignment operator
-# a = 4-2
-# print(a)
-# b = 6
-# b += 3
-# print(b)
+a = 4-2
+print(a)
+b = 6
+b += 3
+print(b)
 
 # # Comparison Operators
 
-# d = 5 < 4
-# d = 5 < 4
-# d = 5 >= 4
-# d = 5 <= 4
-# d = 5 == 4
-# d = 5 != 4
-# print(d)
+d = 5 < 4
+d = 5 < 4
+d = 5 >= 4
+d = 5 <= 4
+d = 5 == 4
+d = 5 != 4
+print(d)
 
 
 # Comparison Operators
-# e = True or False
-# print(e)
+e = True or False
+print(e)
 
 
 # Logical Operator: evaluate multiple condition(or, and, not)
@@ -34,48 +34,48 @@
 #                   not = inverts the conditon (not false, not true)
 
 # # Truth table of 'or'
-# print("True or False is ", True or False)
-# print("True or True is ",True or True)
-# print("Fa1se or True is ",False or True)
-# print("Fa1se or False is ",False or False)
+print("True or False is ", True or False)
+print("True or True is ",True or True)
+print("Fa1se or True is ",False or True)
+print("Fa1se or False is ",False or False)
 
 # # Truth table of 'and'
-# print("True and False is ",True and False)
-# print("True and True is ",True and True)
-# print("Fa1se and True is ",False and True)
-# print("Fa1se and False is ",False and False)
+print("True and False is ",True and False)
+print("True and True is ",True and True)
+print("Fa1se and True is ",False and True)
+print("Fa1se and False is ",False and False)
 
-# print(not(False))
+print(not(False))
 
-# temp = 20
-# is_raining = True
+temp = 20
+is_raining = True
 
-# if temp > 35 or temp < 0 or is_raining :
-#     print("The outdoor event is cancelled")
-# else:
-#     print("The outdoor event is still scheduled ")
+if temp > 35 or temp < 0 or is_raining :
+    print("The outdoor event is cancelled")
+else:
+    print("The outdoor event is still scheduled ")
 
 
 
-# temp = 28
-# is_sunny = False
+temp = 28
+is_sunny = False
 
-# if temp >= 28 and is_sunny :
-#     print("It is HOT outside")
-#     print("It is sunny")
-# elif temp <= 0 & is_sunny:
-#     print("It is cold outside")
+if temp >= 28 and is_sunny :
+    print("It is HOT outside")
+    print("It is sunny")
+elif temp <= 0 & is_sunny:
+    print("It is cold outside")
 
-# elif 28 > temp > 0 and is_sunny:
-#     print("It is warm outside ")
+elif 28 > temp > 0 and is_sunny:
+    print("It is warm outside ")
 
-# elif temp >= 28 and not is_sunny :
-#     print("It is cloudy outside")
-#     print("It is sunny")
-# elif temp <= 0 and not is_sunny:
-#     print("It is cold outside")
+elif temp >= 28 and not is_sunny :
+    print("It is cloudy outside")
+    print("It is sunny")
+elif temp <= 0 and not is_sunny:
+    print("It is cold outside")
 
-# elif 28 > temp > 0 and not is_sunny:
-#     print("It is warm outside ")
+elif 28 > temp > 0 and not is_sunny:
+    print("It is warm outside ")
 
-# Conditional expression
+
